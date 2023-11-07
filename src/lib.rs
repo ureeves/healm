f54a4fc7c4447ace004d6f96391c04381571dc97
Eduardo Leegwater Simões <eduardols@protonmail.com>
2023-11-07 23:39:08 +0100

    Aggregation and insertion

--- src/lib.rs
+++ src/lib.rs
@@ -10,21 +10,25 @@ use alloc::alloc::{Allocator, Global, Layout};
 use core::{mem, ptr};
 
 /// A heap allocated Merkle tree.
-pub struct HamTree<T, const H: usize, const A: usize, Alloc = Global> {
-    ptr: *mut T,
+pub struct HamTree<T, const H: u32, const A: usize, Alloc = Global> {
+    ptr: *mut Node<T>,
     alloc: Alloc,
 }
 
 type Node<T> = Option<T>;
 
-/// Number of nodes in a tree.
-const fn n_tree_nodes<const H: usize, const A: usize>() -> usize {
-    let mut n_nodes = 1; // start with the root
+const fn n_tree_leaves(height: u32, arity: usize) -> usize {
+    arity.pow(height)
+}
 
-    let mut h = H;
-    while h > 0 {
-        n_nodes += tree_levels::<H, A>()[H - h];
-        h -= 1;
+/// Number of nodes in a tree.
+const fn n_tree_nodes(height: u32, arity: usize) -> usize {
+    let mut n_nodes = 0;
+
+    let mut h = 0;
+    while h <= height {
+        n_nodes += n_tree_leaves(height, arity);
+        h += 1;
     }
 
     n_nodes
@@ -32,32 +36,20 @@ const fn n_tree_nodes<const H: usize, const A: usize>() -> usize {
 
 /// Returns the layout of a tree of the given height and arity, together with
 /// the number of nodes in the tree.
-const fn node_layout<T, const H: usize, const A: usize>() -> Layout {
+const fn tree_layout<T>(height: u32, arity: usize) -> Layout {
     let node_size = mem::size_of::<Node<T>>();
     let node_align = mem::align_of::<Node<T>>();
 
-    let n_tree_nodes = n_tree_nodes::<H, A>();
+    let n_tree_nodes = n_tree_nodes(height, arity);
     let tree_size = node_size * n_tree_nodes;
 
     unsafe { Layout::from_size_align_unchecked(tree_size, node_align) }
 }
 
-/// Number of nodes in all levels of a tree of the given height and arity,
-/// ordered from the leaves up to, but excluding, the root.
-const fn tree_levels<const H: usize, const A: usize>() -> [usize; H] {
-    let mut n_nodes = [0; H];
-
-    let mut h = H;
-    #[allow(clippy::cast_possible_truncation)]
-    while h > 0 {
-        n_nodes[H - h] = A.pow(h as u32);
-        h -= 1;
-    }
-
-    n_nodes
-}
-
-impl<T, const H: usize, const A: usize> HamTree<T, H, A> {
+impl<T, const H: u32, const A: usize> HamTree<T, H, A>
+where
+    T: Aggregate<A>,
+{
     /// Construct a new, empty `HamTree<T, H, A>`.
     ///
     /// The tree will not allocate until elements are inserted.
@@ -70,18 +62,15 @@ impl<T, const H: usize, const A: usize> HamTree<T, H, A> {
     }
 }
 
-impl<T, const H: usize, const A: usize, Alloc: Allocator>
-    HamTree<T, H, A, Alloc>
+impl<T, const H: u32, const A: usize, Alloc> HamTree<T, H, A, Alloc>
+where
+    T: Aggregate<A>,
+    Alloc: Allocator,
 {
-    const LAYOUT: Layout = node_layout::<T, H, A>();
-    const LEVELS: [usize; H] = tree_levels::<H, A>();
-
-    // Indexing the level array at 0 means that `H` can never be 0, which allows
-    // us to optimize a check away at insertion. That check would have been
-    // between inserting directly at the root or not.
+    const LAYOUT: Layout = tree_layout::<T>(H, A);
 
     /// The maximum number of leaves a tree can hold.
-    pub const N_LEAVES: usize = Self::LEVELS[0];
+    pub const N_LEAVES: usize = n_tree_leaves(H, A);
 
     /// Construct a new, empty `HamTree<T, H, A>`.
     ///
@@ -103,28 +92,68 @@ impl<T, const H: usize, const A: usize, Alloc: Allocator>
         assert!(index < Self::N_LEAVES, "Index out of bounds");
 
         if self.ptr.is_null() {
-            let ptr = match self.alloc.allocate(Self::LAYOUT) {
+            let ptr = match self.alloc.allocate_zeroed(Self::LAYOUT) {
                 Ok(ptr) => ptr,
                 Err(err) => panic!("Allocation error: {err}"),
             };
             self.ptr = ptr.as_ptr().cast();
         }
 
-        // safety: this dereferences memory that was just allocated previously
-        let popped = unsafe {
+        // safety: the memory was just allocated, and we ensure in the layout
+        // that our calculations never leave the bounds of the allocated object
+        //
+        // # See docs/layout.svg
+        // # https://doc.rust-lang.org/core/ptr/index.html#allocated-object
+        unsafe {
+            let mut level_ptr = self.ptr;
+            let mut index = index;
+
+            // Modify the leaf node
             let mut leaf = Some(leaf);
+            let node_ptr = level_ptr.add(index);
+            let node = &mut *node_ptr.cast();
+            mem::swap(node, &mut leaf);
 
-            let ptr = self.ptr.add(index);
-            let leaf_ref = &mut *ptr.cast::<Node<T>>();
+            // Propagate changes towards the root
+            let mut n_nodes = Self::N_LEAVES;
+            for _ in 0..H {
+                let next_level_ptr = level_ptr.add(n_nodes);
 
-            mem::swap(leaf_ref, &mut leaf);
+                let next_n_nodes = n_nodes / A;
+                let next_index = index / A;
+
+                let siblings_index = A * (index / A);
+                let siblings_ptr = level_ptr.add(siblings_index);
+                let siblings = siblings_ptr.cast();
+
+                let new_parent = T::aggregate(&*siblings);
+                let parent_ptr = next_level_ptr.add(next_index);
+                *parent_ptr = Some(new_parent);
+
+                index = next_index;
+                n_nodes = next_n_nodes;
+
+                level_ptr = next_level_ptr;
+            }
 
             leaf
-        };
+        }
+    }
 
-        todo!("update the tree's hashes");
+    /// Returns the root of the tree.
+    ///
+    /// If no leaves have been inserted, it returns `None`.
+    pub fn root(&self) -> Option<&T> {
+        if self.ptr.is_null() {
+            return None;
+        }
 
-        popped
+        unsafe {
+            let root_index = n_tree_nodes(H, A) - 1;
+            let root_ptr = self.ptr.add(root_index);
+
+            (*root_ptr).as_ref()
+        }
     }
 
     /// The maximum number of leaves the tree can hold.
@@ -134,5 +163,44 @@ impl<T, const H: usize, const A: usize, Alloc: Allocator>
     /// [`N_LEAVES`]: Self::N_LEAVES
     pub const fn capacity(&self) -> usize {
         Self::N_LEAVES
+    }
+}
+
+/// Types that can be aggregated to produce a new value.
+pub trait Aggregate<const A: usize>: Sized {
+    /// Aggregate the given nodes into a new node.
+    fn aggregate(nodes: &[Option<Self>; A]) -> Self;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq)]
+    struct Count(usize);
+
+    impl<const A: usize> Aggregate<A> for Count {
+        fn aggregate(nodes: &[Option<Self>; A]) -> Self {
+            let mut sum = 0;
+            nodes.iter().flatten().for_each(|node| sum += node.0);
+            Self(sum)
+        }
+    }
+
+    #[test]
+    fn insertion() {
+        const H: u32 = 3;
+        const A: usize = 2;
+
+        type Tree = HamTree<Count, H, A>;
+
+        let mut tree = HamTree::<Count, H, A>::new();
+        for i in 0..Tree::N_LEAVES {
+            tree.insert(i, Count(1));
+        }
+
+        println!("{:?}", tree.root());
+
+        assert!(matches!(tree.root(), Some(Count(Tree::N_LEAVES))));
     }
 }
